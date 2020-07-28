ce46af01e0e318dba528dd48efc707113858fdd0
asiskumarroy <asisroylitun@gmail.com>
2020-07-28 09:04:29 +0530

    tuesday work

--- graphs/dfs.cpp
+++ graphs/dfs.cpp
@@ -19,3 +19,79 @@
    * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *  
 */
 
+# include <bits/stdc++.h>
+using namespace std;
+#define MP make_pair
+#define PB push_back
+#define INF (int)1e9
+#define EPS 1e-9
+#define PI 3.1415926535897932384626433832795
+#define MOD 1000000007
+#define read(type) readInt<type>()
+const double pi=acos(-1.0);
+typedef pair<int, int> PII;
+typedef vector<int> VI;
+typedef vector<string> VS;
+typedef vector<PII> VII;
+typedef vector<VI> VVI;
+typedef map<int,int> MPII;
+typedef set<int> SETI;
+typedef multiset<int> MSETI;
+typedef long int int32;
+typedef unsigned long int uint32;
+typedef long long int int64;
+typedef unsigned long long int  uint64;
+
+
+vector< vector<int> > adj;
+vector< bool > used;
+
+void dfs(int s)
+{
+   used[s]=true;
+   cout<<s<<" ";
+   for(auto i:adj[s])
+   {
+      if (used[i]==false)
+      {
+         dfs(i);
+      }
+   }
+}
+
+
+int main()
+{
+   #ifndef ONLINE_JUDGE
+   freopen("input.txt","r",stdin);
+   freopen("output.txt","w",stdout);
+   #endif
+
+   ios_base::sync_with_stdio(false); 
+   cin.tie(NULL);
+
+
+
+   int n,e,s; // nodes,edges and source
+
+   cin>>n>>e>>s;
+
+   adj.assign(n,vector<int>());
+   used.assign(n,false);
+
+   int a,b; // edges
+
+   for(int i=0;i<e;i++)
+   {
+      cin>>a>>b;
+      adj[a].push_back(b);
+      adj[b].push_back(a); //comment in case of directed graph
+   }
+
+   dfs(s);
+   return 0;
+
+}
+
+
+
