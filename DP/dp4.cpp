8299b091ad26dd4fdc89dad5687a641982001378
asiskumarroy <asisroylitun@gmail.com>
2020-07-30 01:06:53 +0530

    Binomial coefficient

---
+++ DP/dp4.cpp
@@ -0,0 +1,87 @@
+/*
+   * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+   *                                                                 *
+   *              *        *            *                            *
+   *            * *      *   *        * *                            * 
+   *           *  *     *     *      *  *                            *
+   *          *   *    *       *    *   *                            * 
+   *         *    *    *       *   *    *                            *
+   *        *     *    *       *  *     *                            *
+   *        * * * * *  *       *  * * * * *                          *
+   *              *    *       *        *                            * 
+   *              *     *     *         *                            *
+   *              *        *            *                            *
+   *                                                                 *
+   *        E X P E R T   N O T   F O U N D                          *
+   *                                                                 *
+   *  Get introspected by your ethics, judged by your deeds          *
+   *                                                                 *
+   * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *  
+*/
+
+# include <bits/stdc++.h>
+using namespace std;
+#define MP make_pair
+#define PB push_back
+#define INF (int)1e9
+#define EPS 1e-9
+#define PI 3.1415926535897932384626433832795
+#define MOD 1000000007
+#define read(type) readInt<type>()
+const double pi=acos(-1.0);
+typedef pair<int, int> PII;
+typedef vector<int> VI;
+typedef vector<string> VS;
+typedef vector<PII> VII;
+typedef vector<VI> VVI;
+typedef map<int,int> MPII;
+typedef set<int> SETI;
+typedef multiset<int> MSETI;
+typedef long int int32;
+typedef unsigned long int uint32;
+typedef long long int int64;
+typedef unsigned long long int  uint64;
+
+
+
+
+int main()
+{
+      
+   #ifndef ONLINE_JUDGE
+   freopen("input.txt","r",stdin);
+   freopen("output.txt","w",stdout);
+   #endif
+
+   ios_base::sync_with_stdio(false); 
+   cin.tie(NULL);
+
+   int n,k;
+   cin>>n;
+   cin>>k;
+
+   vector< vector<int> > vect(k+1,vector<int>(n+1,0));
+
+   for(int i=0;i<vect.size();i++)
+   vect[i][0]=0;
+
+   for(int j=1;j<vect[0].size();j++)
+   vect[0][j]=1;
+
+   for(int i=1;i<=k;i++)
+   {
+      for(int j=1;j<=n;j++)
+      {
+         if(i==j)
+         vect[i][j]=1;
+         else
+         {
+            vect[i][j]=vect[i-1][j-1]+vect[i][j-1];
+         }
+      }
+   }
+   cout<<vect[k][n];
+
+   return 0;
+
+}
