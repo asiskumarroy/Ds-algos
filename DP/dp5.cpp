b3e15378ca3d1f995d4a301b7df4f560ab0ab2d2
asiskumarroy <asisroylitun@gmail.com>
2020-07-30 10:48:46 +0530

    Egg dropping problem

---
+++ DP/dp5.cpp
@@ -0,0 +1,88 @@
+/*
+   * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+   *                                                                 *
+   *              *        *            *                            *
+   *            * *      *   *        * *                            * 
+   *           *  *     *     *      *  *                            *
+   *          *   *    *       *    *   *                            * 
+   *         *    *    *       *   *    *                            *
+   *        *     *    *       *  *     *                            *
+   *        * * * * *  *       *  * * * * *                          *
+   *              *    *       *        *                            * 
+   *              *     *     *         *                            *
+   *              *        *            *                            *
+   *                                                                 *
+   *        E X P E R T   N O T   F O U N D                          *
+   *                                                                 *
+   *  Get introspected by your ethics, judged by your deeds          *
+   *                                                                 *
+   * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *  
+*/
+
+# include <bits/stdc++.h>
+using namespace std;
+#define MP make_pair
+#define PB push_back
+#define INF (int)1e9
+#define EPS 1e-9
+#define PI 3.1415926535897932384626433832795
+#define MOD 1000000007
+#define read(type) readInt<type>()
+const double pi=acos(-1.0);
+typedef pair<int, int> PII;
+typedef vector<int> VI;
+typedef vector<string> VS;
+typedef vector<PII> VII;
+typedef vector<VI> VVI;
+typedef map<int,int> MPII;
+typedef set<int> SETI;
+typedef multiset<int> MSETI;
+typedef long int int32;
+typedef unsigned long int uint32;
+typedef long long int int64;
+typedef unsigned long long int  uint64;
+
+
+
+
+int main()
+{
+      
+   #ifndef ONLINE_JUDGE
+   freopen("input.txt","r",stdin);
+   freopen("output.txt","w",stdout);
+   #endif
+
+   ios_base::sync_with_stdio(false); 
+   cin.tie(NULL);
+
+   int n,k;
+   cin>>n;
+   cin>>k;
+
+   vector< vector<int> > vect(n+1,vector<int>(k+1,0));
+
+   for(int i=1;i<vect.size();i++)
+   vect[i][1]=1;
+
+   for(int j=1;j<vect[0].size();j++)
+   vect[1][j]=j;
+
+   for(int i=2;i<=n;i++)
+   {
+      for(int j=2;j<=k;j++)
+      {
+         vect[i][j]=INT_MAX;
+         for(int k=1;k<=j;k++)
+         {
+            vect[i][j] = min(vect[i][j],1+max(vect[i-1][k-1],vect[i][j-k]));
+         }
+         
+      }
+   }
+
+   cout<<vect[n][k];
+
+   return 0;
+
+}
