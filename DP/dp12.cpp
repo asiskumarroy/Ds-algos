fdbbf9869df9e4c0dee6f2f56231134c5fe2f11b
asiskumarroy <asisroylitun@gmail.com>
2020-08-18 18:35:24 +0530

    staircase fibonacci dp

---
+++ DP/dp12.cpp
@@ -0,0 +1,84 @@
+/*
+   * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+   *                                                                 *
+   *              *        *            *                            *
+   *            * *      *   *        * *                            * 
+   *           *  *     *     *      *  *                            *
+   *          *   *    *       *    *   *                            * 
+   *         *    *    *       *   *    *                            *
+   *        *     *    *       *  *     *                            *
+   *        * * * * *  *       *  * * * * *                          *
+   *              *    *       *        *                            * 
+   *              *     *     *         *                            *
+   *              *        *            *                            *
+   *                                                                 *
+   *        E X P E R T   N O T   F O U N D                          *
+   *                                                                 *
+   *  Get introspected by your ethics, judged by your deeds          *
+   *                                                                 *
+   * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *  
+*/
+
+
+# include <bits/stdc++.h>
+using namespace std;
+#define MP make_pair
+#define PB push_back
+#define INF (int)1e9
+#define EPS 1e-9
+#define PI 3.1415926535897932384626433832795
+#define MOD 1000000007
+#define read(type) readInt<type>()
+const double pi=acos(-1.0);
+typedef pair<int, int> PII;
+typedef vector<int> VI;
+typedef vector<string> VS;
+typedef vector<PII> VII;
+typedef vector<VI> VVI;
+typedef map<int,int> MPII;
+typedef set<int> SETI;
+typedef multiset<int> MSETI;
+typedef long int int32;
+typedef unsigned long int uint32;
+typedef long long int int64;
+typedef unsigned long long int  uint64;
+
+
+
+
+int main()
+{
+
+  vector <long long int > vect(100001,0);
+
+   vect[1]=1;
+   vect[2]=2;
+  for(long long int i=3;i<100001;i++)
+  {
+    vect[i]=((vect[i-1] % MOD) + (vect[i-2] % MOD)) % MOD;
+  } 
+
+
+   #ifndef ONLINE_JUDGE
+   freopen("input.txt","r",stdin);
+   freopen("output.txt","w",stdout);
+   #endif
+
+   ios_base::sync_with_stdio(false); 
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    long long int t;
+    cin>>t;
+
+    while(t--)
+    {
+      long long int n;
+      cin>>n;
+      cout<<vect[n]<<"\n";
+
+    }
+      
+      
+    return 0;
+}
